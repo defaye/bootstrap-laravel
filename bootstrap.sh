e55bac04e4ac041b4a1f2c1b36fbddf793e41b20
Jonathan de Faye <defaye@users.noreply.github.com>
2017-03-20 21:27:42 +0000

    chmod +x permissions.sh if it exists i.a...
    Generate a new site key if the .environment file was instantiated.

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,18 +19,24 @@ sudo -Eu root bash -c 'chown $LARAVEL_USER:www-data bootstrap/cache'
 sudo -Eu root bash -c 'find storage -type d -exec chmod 777 {} \;'
 sudo -Eu root bash -c 'find storage -type f ! -name .gitignore -exec chmod 644 {} \;'
 
-if [ ! -e .env ]
-then
-    cp .env.example .env
-    echo "Initialised the example environment file..."
-fi
-
 sudo -Eu root bash -c 'chmod 750 ./'
 sudo -Eu root bash -c 'chown $LARAVEL_USER:www-data ./'
-sudo -Eu root bash -c 'chmod +x permissions.sh'
+
+if [ -e .env ]
+then
+    sudo -Eu root bash -c 'chmod +x permissions.sh'
+fi
 
 composer dumpautoload
 composer install
+
+if [ ! -e .env ]
+then
+    cp .env.example .env
+    php artisan key:generate
+    echo "Initialised the example environment file..."
+fi
+
 php artisan down
 php artisan optimize
 php artisan up
